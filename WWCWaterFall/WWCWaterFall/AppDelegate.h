46080e03422be2c109958731459e47462950ca54
wangweichao <4554634698@qq.com>
2017-02-22 17:12:01 +0800

    初始化项目

---
+++ WWCWaterFall/WWCWaterFall/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WWCWaterFall
+//
+//  Created by WeiChaoW on 2017/2/22.
+//  Copyright © 2017年 WeiChaoW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WWCWaterFall/WWCWaterFall/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WWCWaterFall
+//
+//  Created by WeiChaoW on 2017/2/22.
+//  Copyright © 2017年 WeiChaoW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
